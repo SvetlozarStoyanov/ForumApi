8dfc8e30439ee9769a807b7f8172996b25946a4a
SvetlozarStoyanov <svetlio214@gmail.com>
2024-12-10 21:10:36 +0200

    Minor tweak for adding header which indicates that a JWT is expired

--- ForumApi/Middleware/ExpiredTokenMiddleware.cs
+++ ForumApi/Middleware/ExpiredTokenMiddleware.cs
@@ -21,7 +21,7 @@ namespace ForumApi.Middleware
             {
                 if (!context.Response.HasStarted)
                 {
-                    context.Response.Headers.Add("Token-Expired", "true");
+                    context.Response.Headers.Append("Token-Expired", "true");
                     context.Response.StatusCode = StatusCodes.Status401Unauthorized;
                     await context.Response.WriteAsync("Token has expired");
                 }
